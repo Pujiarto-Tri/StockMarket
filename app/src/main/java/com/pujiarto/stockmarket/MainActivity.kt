818b6d7ddc5dc01cd8a59d8df73f12d7dbac910f
pujiarto <pujiarto.ta@gmail.com>
2022-07-12 10:31:28 +0800

    Added Company Info and stock chart screen

--- app/src/main/java/com/pujiarto/stockmarket/MainActivity.kt
+++ app/src/main/java/com/pujiarto/stockmarket/MainActivity.kt
@@ -7,8 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
-import com.pujiarto.stockmarket.presentation.company_listings.NavGraph
-import com.pujiarto.stockmarket.presentation.company_listings.NavGraphs
+import com.pujiarto.stockmarket.presentation.NavGraphs
 import com.pujiarto.stockmarket.ui.theme.StockMarketTheme
 import com.ramcosta.composedestinations.DestinationsNavHost
 import dagger.hilt.android.AndroidEntryPoint
